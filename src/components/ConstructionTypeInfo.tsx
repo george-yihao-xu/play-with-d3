8b60657e49c4bbbb649a795cdb0c345c6b3a0ee7
george-yihao-xu <gxu@sasaki.com>
2024-10-11 21:36:38 -0400

    pie on click

--- src/components/ConstructionTypeInfo.tsx
+++ src/components/ConstructionTypeInfo.tsx
@@ -1,12 +1,20 @@
 import { useProjectContext } from "../context/ProjectContext";
 import projects from "../dummyData/projects";
+import colors from "../dummyData/colors";
 
 export const ConstructionTypeInfo = () => {
     const { activeConstructionType } = useProjectContext();
 
     return <div>
         <h2>Construction Type Info</h2>
-        <p>Active Construction Type: <b>{activeConstructionType}</b></p>
+        <p>Active Construction Type: <b style={{ color: colors.constructionType[activeConstructionType as keyof typeof colors.constructionType] }}>{activeConstructionType}</b></p>
         <p>Number of projects: <b>{projects.filter(project => project.constructionType === activeConstructionType).length}</b></p>
+        <ul>
+            {projects.filter(project => project.constructionType === activeConstructionType).map(project => (
+                <li key={project.id}>{project.name}</li>
+            ))}
+        </ul>
     </div>
 }
+
+export default ConstructionTypeInfo;
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -1,5 +1,6 @@
 import { useProjectContext } from "../context/ProjectContext";
 import { Project } from "../dummyData/projects";
+import colors from "../dummyData/colors";
 
 interface ProjectListProps {
     projects: Project[];
@@ -12,7 +13,7 @@ export const ProjectList = ({ projects }: ProjectListProps) => {
     return <div style={{ width: "300px", display: "flex", flexDirection: "column", gap: "10px" }}>
         <h2>Project List</h2>
         <p>Click on a project to select it</p>
-        <p>Selected Project: <b>{selectedProject?.name}</b>, whose construction type is <b>{selectedProject?.constructionType}</b></p>
+        <p>Selected Project: <b>{selectedProject?.name}</b>, whose construction type is <b style={{ color: colors.constructionType[selectedProject?.constructionType as keyof typeof colors.constructionType] }}>{selectedProject?.constructionType}</b></p>
         <ul>
             {projects.map((project) => (
                 <li style={{ marginBottom: "10px", cursor: "pointer" }} key={project.id} onClick={() => {
